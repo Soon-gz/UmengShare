5626e9135d2d1a1558ffd8b8e6975d5df2f31106
SingleShu <sw201202@126.com>
2016-06-21 10:08:22 +0800

    提交微信的key和id

--- app/src/main/java/com/abings/umengsharedemo/MyApplication.java
+++ app/src/main/java/com/abings/umengsharedemo/MyApplication.java
@@ -11,7 +11,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        PlatformConfig.setWeixin("wxe5d412509c6a0c30", "d6ac9a4f059f863bc1f532b50797e7f8");
+        PlatformConfig.setWeixin("wx62eaa747fa4f9993", "b3a1440ddcd56a58f11baf2d0115f29f");
         //微信 appid appsecret
         PlatformConfig.setSinaWeibo("3136860142","ba40ec9c01bdbae106d20ab57b62fb0b");
         //新浪微博 appkey appsecret
